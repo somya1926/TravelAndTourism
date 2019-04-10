eca56ff300fe597c28bbd69c9718496780f004df
Elanza-48 <rishikundu6@gmail.com>
2019-04-10 23:42:14 +0530

    [Maintenance] Very small UI and bug fixes.

--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -28,11 +28,11 @@ $(function() {
 		changeYear: true,
 		minDate:'m',
 		showButtonPanel: true,
-		/*onClose: function(dateText, inst) {
+		onClose: function(dateText, inst) {
 			var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
 			var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
 			$(this).datepicker('setDate', new Date(year, month, 1));
-		}*/
+		}
 	});
 });
 
