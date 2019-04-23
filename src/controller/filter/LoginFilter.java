fa3efc14d183f55fe4f859f4d6097579f2479e3a
Elanza-48 <rishikundu6@gmail.com>
2019-04-23 21:08:47 +0530

    [Maintenance] Changed Database path and minor bug fixes.
    (cherry picked from commit e77ec7e2f440c1c1690a3c4d0df724c7ee4cd75f)

--- src/controller/filter/LoginFilter.java
+++ src/controller/filter/LoginFilter.java
@@ -194,8 +194,7 @@ public class LoginFilter implements Filter {
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		String[] part= (fConfig.getServletContext().getRealPath("/").split("\\.metadata"));
-		ConnectionFactory.setDBpath(part[0]+fConfig.getServletContext().getContextPath()+"\\database");
+		ConnectionFactory.setDBpath(fConfig.getServletContext().getRealPath("/WEB-INF/database"));
 	}
 
 }
